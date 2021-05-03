ac9f2b8ad72885b7825ab01a3359da645df437e2
Akiyoshi7922 <1603akiyoshi2045kojima@gmail.com>
2021-05-03 18:59:39 +0900

    initial commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -38,6 +38,15 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def destroy
+    # binding.pry
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
@@ -47,7 +56,7 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
-  # def contributor_confirmation
-  #   redirect_to root_path unless current_user == @prototype.user
-  # end
+  def confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
 end
